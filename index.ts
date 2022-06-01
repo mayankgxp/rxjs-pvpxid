13525a5ebcbb9c59925604894dd36445df05f5ba
ARI\Mayank.Gupta <mayank.gupta@leadventure.com>
2022-06-01 14:31:28 +0530

    Replay Subject

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Subject, BehaviorSubject } from 'rxjs';
+import { Subject, BehaviorSubjec, ReplaySubject } from 'rxjs';
 
 //#region  Subject...
   // const subject = new Subject<number>();
@@ -15,7 +15,24 @@ import { Subject, BehaviorSubject } from 'rxjs';
 //#endregion
 
 //#region  Behavioral Subject
-const subject = new BehaviorSubject(0); // 0 is the initial value
+// const subject = new BehaviorSubject(0); // 0 is the initial value
+
+// subject.subscribe({
+//   next: (v) => console.log(`observerA: ${v}`),
+// });
+
+// subject.next(1);
+// subject.next(2);
+
+// subject.subscribe({
+//   next: (v) => console.log(`observerB: ${v}`),
+// });
+
+// subject.next(3);
+//#endregion
+
+//#region Replay Subject
+const subject = new ReplaySubject(3); // buffer 3 values for new subscribers
 
 subject.subscribe({
   next: (v) => console.log(`observerA: ${v}`),
@@ -23,10 +40,12 @@ subject.subscribe({
 
 subject.next(1);
 subject.next(2);
+subject.next(3);
+subject.next(4);
 
 subject.subscribe({
   next: (v) => console.log(`observerB: ${v}`),
 });
 
-subject.next(3);
+subject.next(5);
 //#endregion
