350af4e2496dbf4dcb6a6554cf3f839cd6292bcd
ARI\Mayank.Gupta <mayank.gupta@leadventure.com>
2022-06-01 14:29:09 +0530

    BehaviorSubject

--- index.ts
+++ index.ts
@@ -1,17 +1,32 @@
-import { Subject } from 'rxjs';
+import { Subject, BehaviorSubject } from 'rxjs';
 
 //#region  Subject...
-const subject = new Subject<number>();
+  // const subject = new Subject<number>();
+
+  // subject.subscribe({
+  //   next: (v) => console.log(`observerA: ${v}`),
+  // });
+  // subject.subscribe({
+  //   next: (v) => console.log(`observerB: ${v}`),
+  // });
+
+  // subject.next(1);
+  // subject.next(2);
+//#endregion
+
+//#region  Behavioral Subject
+const subject = new BehaviorSubject(0); // 0 is the initial value
 
 subject.subscribe({
   next: (v) => console.log(`observerA: ${v}`),
 });
+
+subject.next(1);
+subject.next(2);
+
 subject.subscribe({
   next: (v) => console.log(`observerB: ${v}`),
 });
 
-subject.next(1);
-subject.next(2);
+subject.next(3);
 //#endregion
-
-//Behavioral Subject
