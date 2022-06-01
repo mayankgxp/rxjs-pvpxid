5a46768612a468a4f7de46eb39619e8c9ddd69f3
mayankgxp <mayankg303@gmail.com>
2022-06-01 13:47:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-pvpxid
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-pvpxid)
---
+++ index.ts
@@ -0,0 +1,16 @@
+import { Subject } from 'rxjs';
+
+// Subject...
+const subject = new Subject<number>();
+
+subject.subscribe({
+  next: (v) => console.log(`observerA: ${v}`),
+});
+subject.subscribe({
+  next: (v) => console.log(`observerB: ${v}`),
+});
+
+subject.next(1);
+subject.next(2);
+
+//Behavioral Subject
